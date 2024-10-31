d2a776f2a4c0b4394ae129ee77f338a6d3d0b47c
Fahrisajalah <fahrigryn97@gmail.com>
2024-10-31 11:13:25 +0700

    >>>>>>

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ Route::get('/games', [App\Http\Controllers\LandingControllers::class, 'games']);
 Route::get('/mediasosial', [App\Http\Controllers\LandingControllers::class, 'mediasosial']);
 Route::get('/aboutus', [App\Http\Controllers\LandingControllers::class, 'aboutus']);
 Route::get('/pendidikan', [App\Http\Controllers\LandingControllers::class, 'pendidikan']);
-<<<<<<< HEAD
+>>>>>> HEAD
 Route::get('/pendidikan', [App\Http\Controllers\LandingControllers::class, 'pendidikan']);
 =======
 Route::get('/login', [SesiController::class, 'login']);
