f7b130102549784d74751c049e6f539e5a8fa1d5
farizhkm <farizhakimz7@gmail.com>
2024-12-19 10:18:55 -0600

    penyempurnaan tes cpns

--- resources/views/landing/pengetahuan.blade.php
+++ resources/views/landing/pengetahuan.blade.php
@@ -752,6 +752,7 @@ main {
         <link href="assets/vendor/aos/aos.css" rel="stylesheet">
         <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
         <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
       
         <!-- Main CSS File -->
       
